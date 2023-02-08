7bcdd77a5e0468ba3cbf76fd34dcd632b9242989
Kiril Jovchev <kiril.jovchev@siili.com>
2023-02-09 00:38:47 +0200

    Added reading and writing to file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var net = require('net');
 
+var skiers = {}
+skiers["100"] = {Name:"First 100"}
+skiers["101"] = {Name:"First 101"}
+
+
 var client = new net.Socket();
+const fs = require('fs');
+const fileName = "c:/Projects/ski/skier.txt"
 client.connect(5409, '127.0.0.1', function() {
 	console.log('Connected');
 	client.write('Hello, server! Love, Client.');
@@ -18,9 +25,20 @@ client.on('data', function(data) {
         var timing = str.split(';')
         var bib = timing[3]
         var status = timing[1]
+        var skier = skiers[bib]
         var runTime = timing[6]
-        console.log(str)
-        console.log("Bib: " + bib + " status: " + status + " time " + SkiToTime(runTime))
+        var content = ""
+        if (status === '0'){
+            skier.StartTime = SkiToSecondsDec(runTime)
+            content = "[" + bib + "] " + skier.Name 
+        }
+        else {
+            skier.EndTime = SkiToSecondsDec(runTime)
+            content = "[" + bib + "] " + skier.Name + " --- " + (skier.EndTime - skier.StartTime).toFixed(2);
+        }
+        console.log(content)
+        fs.writeFile(fileName, content, { flag: 'w+' }, err => {});
+        //console.log(str);
 
     }
         
@@ -30,14 +48,19 @@ client.on('close', function() {
 	console.log('Connection closed');
 });
 
-function SkiToTime (duration)
+function SkiToSecondsDec(duration)
+{
+    return parseFloat(SkiToTime(duration))
+}
+
+function SkiToTime(duration)
 {
     var ms = duration.slice(-6)
     var seconds = duration.slice(0, duration.length-6)
-    return sToTime(seconds) + "." + ms
+    return seconds + "." + ms
 }
 
-function sToTime(duration) {
+function secToTime(duration) {
     var seconds = Math.floor((duration) % 60),
       minutes = Math.floor((duration / 60) % 60),
       hours = Math.floor((duration /(60 * 60)) % 24);
